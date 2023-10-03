4043b8afac2b43560af200618ef288e64df7be8f
Facundo Rios <Facundorios2005@gmail.com>
2023-10-02 23:49:32 -0300

    feat: added Jsonwebtoken function

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,25 +1,33 @@
 //Importations
 import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
-
   try {
+    const passwordHash = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
-    res.json(userSaved);
-    
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).send({ message: error.message });
   }
 };
 
 export const login = (req, res) => {
   res.send("login");
 };
- 
