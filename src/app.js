f1ab19cb764b2df89831861dc6df45d6935e0d36
Facundorios05 <Facundorios2005@gmail.com>
2023-10-04 11:47:04 -0600

    feat: Tokens validations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-//Importations
+//Dependencies importations
 import express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
+//Routes importation
 import authRoutes from './routes/auth.routes.js'
 
 const app = express()
@@ -8,6 +11,8 @@ const app = express()
 //Initializations
 app.use(express.json())
 app.use(morgan('dev'));
+app.use(cookieParser())
+
 app.use('/api', authRoutes);
 
 
---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,20 @@
+import Jwt from "jsonwebtoken";
+import { TOKEN_SECRET_KEY } from "../config.js"; 
+
+export const authRequired = (req, res, next) => {
+    const { token  } = req.cookies;
+
+    if (!token) 
+    return res.status(401).json({
+        message: 'No token, autorization denied'
+    })
+
+    Jwt.verify(token, TOKEN_SECRET_KEY, (err, user) => {
+        if(err) return res.status(403).json({ message: "Invalid token"});   
+        
+        req.user = user
+        next();
+    })
+
+}
+
