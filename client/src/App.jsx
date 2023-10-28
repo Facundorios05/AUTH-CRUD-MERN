0883f82a770a10430e64f6df985ff1bf3982e5ab
Facundo Ríos <Facundorios2005@gmail.com>
2023-10-28 19:58:25 -0300

    feat: user context #1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthConext";
 
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/login" element={ <LoginPage /> } />
-        <Route path="/register" element={ <RegisterPage />} />
-        <Route path="/tasks" element={<h1>Tasks Page</h1>} />
-        <Route path="/add-task" element={<h1>Add task Page</h1>} />
-        <Route path="/tasks/:id" element={<h1>Task id Page</h1>} />
-        <Route path="/profile" element={<h1>Profile Page</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+          <Route path="/add-task" element={<h1>Add task Page</h1>} />
+          <Route path="/tasks/:id" element={<h1>Task id Page</h1>} />
+          <Route path="/profile" element={<h1>Profile Page</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,12 +1,14 @@
 import { useForm } from "react-hook-form";
-import { registerRequest } from "../api/auth";
+
+import { useAuth } from "../context/AuthConext";
 
 function RegisterPage() {
   const { register, handleSubmit } = useForm();
-
+  const { singup, user } = useAuth();
+  
+  console.log(user)
   const onSubmit = handleSubmit(async (values) => {
-    const res = await registerRequest(values);
-    console.log(res);
+    singup(values)
   });
 
   return (
